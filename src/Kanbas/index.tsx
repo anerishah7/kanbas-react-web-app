47b613269387a966c60f4c81589a260a80016c43
Aneri Shah <anerishah@Aneris-MacBook-Air.local>
2024-11-21 19:53:40 -0500

    Enrollment done

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,7 +5,7 @@ import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
 import "./styles.css";
 import { useEffect, useState } from "react";
-import { Provider, useSelector } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import ProtectedRoute from "./Account/ProtectedRoute";
 import ProtectedRouteCourse from "./Account/ProtectedRouteCourse"; 
 import Session from "./Account/Session";
@@ -13,7 +13,9 @@ import * as userClient from "./Account/client";
 import * as courseClient from "./Courses/client";
 
 export default function Kanbas() {
+  const dispatch = useDispatch();
   const [courses, setCourses] = useState<any[]>([]);
+  const [all_courses, setAllCourses] = useState<any[]>([]);
   const [course, setCourse] = useState<any>({
     _id: "1234", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15", description: "New Description",
@@ -42,19 +44,32 @@ export default function Kanbas() {
   const [enrollment, setEnrollment] = useState<any>({
     _id: "1234", user: "New User", course: "New Course",
   });
-  const addNewEnrollment = (course_id: any, user_id:any) => {
-    setEnrollments([...enrollments, { ...enrollment, _id: new Date().getTime().toString(), 
-      course: course_id, user: user_id }]);
-  };
-  const deleteEnrollment = (course_id: any, user_id:any) => {
+  // const addNewEnrollment = (user_id: any, course_id:any) => {
+  //   setEnrollments([...enrollments, { ...enrollment, _id: new Date().getTime().toString(), 
+  //     course: course_id, user: user_id }]);
+  // };
+  const removeEnrollment = async (user_id: string, course_id: string) => {
+    await userClient.deleteEnrollment(user_id, course_id);
     setEnrollments(enrollments.filter((enrollment) => 
       !(enrollment.course === course_id && enrollment.user === user_id)));
   };
+  const saveEnrollment = async (user_id: any, course_id:any) => {
+    const newEnrollment = await userClient.addEnrollment(user_id, course_id);
+    setEnrollments([...enrollments, newEnrollment]);
+  };
+  // const deleteEnrollment = (user_id: any, course_id:any) => {
+  //   setEnrollments(enrollments.filter((enrollment) => 
+  //     !(enrollment.course === course_id && enrollment.user === user_id)));
+  // };
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const fetchCourses = async () => {
     try {
       const courses = await userClient.findMyCourses();
+      const all_courses = await userClient.findAllCourses();
+      const all_enrollments = await userClient.findAllEnrollments();
       setCourses(courses);
+      setAllCourses(all_courses);
+      setEnrollments(all_enrollments);
     } catch (error) {
       console.error(error);
     }
@@ -74,14 +89,15 @@ export default function Kanbas() {
                   <ProtectedRoute>
                     <Dashboard
                       courses={courses}
+                      all_courses={all_courses}
                       course={course}
                       setCourse={setCourse}
                       addNewCourse={addNewCourse}
                       deleteCourse={deleteCourse}
                       updateCourse={updateCourse}
                       enrollments={enrollments}
-                      addNewEnrollment={addNewEnrollment}
-                      deleteEnrollment={deleteEnrollment}
+                      addNewEnrollment={saveEnrollment}
+                      deleteEnrollment={removeEnrollment}
                       />
                   </ProtectedRoute>
                 } />
